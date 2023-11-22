ab1d54b4fbcd083c1f24f94e7a5cebbe9c451636
Jolmed26 <pemed49es@gmail.com>
2023-11-21 19:01:10 -0600

    Initial commit

---
+++ multiplica.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def multiplica(a,b): 
+    variable1=a*2
+    variable2=b*3
+    resultado=variable1*variable2
+    print("Empezando for ")
+    for i in range(1, 5): 
+        print(i)
+        resultado=resultado * i
+        
+        print(resultado)
+    
+multiplica(3,6)
